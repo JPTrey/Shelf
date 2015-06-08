086c00cb02117d3d1934569fa8ece6dc8ad9e512
JPTrey <jonpaul.simonelli@gmail.com>
2015-06-08 19:34:15 -0400

    updated navbar

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -30,14 +30,10 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="#">Library</a>
+				<a class="navbar-brand" href="#">Shelf</a>
 			</div>
 
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-				<ul class="nav navbar-nav">
-					<li><a href="{{ url('/') }}">Home</a></li>
-				</ul>
-
 				<ul class="nav navbar-nav navbar-right">
 					@if (Auth::guest())
 						<li><a href="{{ url('/auth/login') }}">Login</a></li>
