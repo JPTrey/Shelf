5ef1881d533accd9a1f0d70101e1b42799a2b2d8
JPTrey <jonpaul.simonelli@gmail.com>
2015-06-09 19:23:09 -0400

    test

--- resources/views/user/settings.blade.php
+++ resources/views/user/settings.blade.php
@@ -1,4 +1,21 @@
 @extends('app')
+@section('title') TEST @stop
+@section('pre-scripts') <link rel="stylesheet" type="text/css" href="{{url('/css/style.css')}}"> @stop
 
+@section('content')
+
+<div class="container">
+	<h1 class="text-center">{{$title}}</h1>
+	<div class="row">
+		<div class="col-md-9">
+			@foreach ($text as $element)
+			<?php echo $element ?>
+			@endforeach
+		</div>		
+		<div class="col-md-3">
+			<h4>Toolbar</h4>
+		</div>
+	</div>
+</div>
 
 @stop
